2d8e1d9d914b222191601fb6b177a25f8eed5076
marinacatbytes <marina.kim@catbytes.io>
2024-11-28 09:10:17 +0000

    removes console.log

--- aws/ssm-helper.js
+++ aws/ssm-helper.js
@@ -19,7 +19,6 @@ async function loadSecrets(region, names, withDecryption = false) {
         }, {});
 
         console.log('Fetched secrets:', Object.keys(secrets));
-	console.log('Secrets temp:', secrets);
         return secrets;
     } catch (error) {
         console.error('Error fetching parameters:', error);
