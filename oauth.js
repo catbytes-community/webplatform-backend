8bde7262412d17bd12d58b9e78bbc5d2f5f7c2bf
vm2052 <vvvvictoria2000@gmail.com>
2025-04-21 21:12:55 +0300

    integrating discord oauth

---
+++ oauth.js
@@ -0,0 +1,32 @@
+const { loadSecrets } = require("./aws/ssm-helper");
+const config = require('config');
+
+const discordAuth = {
+  discordClientId: null,
+  discordClientSecret: null,
+  discordRedirectUrl: null,
+};
+
+async function initOAuth() {
+  let discordClientId, discordClientSecret, discordRedirectUrl = null;
+
+  if (process.env.ENVIRONMENT === "local") {
+    discordClientId = process.env.DISCORD_CLIENT_ID;
+    discordClientSecret = process.env.DISCORD_CLIENT_SECRET;
+    discordRedirectUrl = process.env.DISCORD_REDIRECT_URI;
+  } else {
+    const params = await loadSecrets(config.aws.param_store_region, ['/catbytes_webplatform/oauth_creds'], true, true);
+    console.log(params);
+    discordClientId = params['oauth_creds']['discord']['clientID'];
+    discordClientSecret = params['oauth_creds']['discord']['clientSecret'];
+    discordRedirectUrl = params['oauth_creds']['discord']['redirectURIs'][0];
+  }
+
+  discordAuth.discordClientId = discordClientId;
+  discordAuth.discordClientSecret = discordClientSecret;
+  discordAuth.discordRedirectUrl = discordRedirectUrl;
+  console.log(discordAuth);
+}
+
+module.exports = { discordAuth, initOAuth };
+  
--- routes/discord.js
+++ routes/discord.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const discordService = require('../services/discord_bot_service');
+const userService = require("../services/user_service");
 const { respondWithError } = require("./helpers");
 const { verifyRole } = require("../middleware/authorization");
 const { ROLE_NAMES } = require("../utils");
+
+const {discordAuth} = require ("../oauth.js");
+const axios = require('axios');
+const querystring = require('querystring');
 
 const router = express.Router();
 router.use(express.json());
@@ -16,6 +21,55 @@ router.post('/generate-invite', verifyRole(ROLE_NAMES.member), async (req, res) 
     const status = error.status || 500;
     respondWithError(res, status, error.message);
   } 
+});
+
+router.get('/auth/discord', (req, res) => {
+  res.redirect(`https://discord.com/api/oauth2/authorize?client_id=${discordAuth.discordClientId}
+    &redirect_uri=${encodeURIComponent(discordAuth.discordRedirectUrl)}&response_type=code&scope=identify%20email`);
+});
+
+router.get('/auth/discord/callback', async (req, res) => {
+  const { code } = req.query;
+  
+  if (!code) return  respondWithError(res, 400, 'no code provided');
+  
+  try {
+
+    // exchange code for access token
+    const tokenResponse = await axios.post('https://discord.com/api/oauth2/token',
+      querystring.stringify({
+        client_id: discordAuth.discordClientId,
+        client_secret: discordAuth.discordClientSecret,
+        grant_type: 'authorization_code',
+        code: code,
+        redirect_uri: discordAuth.discordRedirectUrl,
+        scope: 'identify email'
+      }), {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      });
+      
+    // get user data from discord
+    const userResponse = await axios.get('https://discord.com/api/users/@me', {
+      headers: {
+        Authorization: `Bearer ${ tokenResponse.data.access_token}`
+      }
+    });
+      
+    const discordUser = userResponse.data;   
+    
+    console.log(discordUser);
+    const user = await userService.getUserByEmail(discordUser.email);
+
+    console.log(user);
+    res.cookie('userUID', user.firebaseId, { httpOnly: true, secure: true, sameSite: 'none' });
+    res.status(200).json({ user: user });
+      
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Authentication failed');
+  }
 });
 
 module.exports = router;
