597b51fdc8af75e058cd1b4fa01aa537370808c6
aliona matveeva <yellalena@gmail.com>
2025-10-05 17:59:50 +0200

    added unit tests

---
+++ tests/routes/s3.test.js
@@ -0,0 +1,69 @@
+const request = require('supertest');
+
+const app = require('../../appForTests');
+
+jest.mock('../../aws/s3_client', () => {;
+  const actual = jest.requireActual('../../aws/s3_client');
+  return {
+    ...actual,
+    generateUploadUrl: jest.fn((bucketPrefix, filename, contentType) => {
+      return {
+        url: `https://mocked-s3-url/${bucketPrefix}/${filename}.mov`,
+        filename: `${filename}.mov`,
+      };
+    })
+  };
+});
+
+describe('POST /presigned-url', () => {
+  it('Successful presigned URL creation', async () => {
+    const res = await request(app)
+      .post('/presigned-url')
+      .send({ 
+        objectKey: '61a54a37-6c7a-466a-8100-a6b09daecded',
+        contentType: 'video/quicktime',
+        objectType: 'application_video'
+      });
+    
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toHaveProperty('url');
+    expect(res.body.url).toBe('https://mocked-s3-url/videos/applications/61a54a37-6c7a-466a-8100-a6b09daecded.mov');
+    expect(res.body).toHaveProperty('filename');
+    expect(res.body.filename).toBe('61a54a37-6c7a-466a-8100-a6b09daecded.mov');
+  });
+
+  it('Return 400 on incomplete payload data', async () => {
+    const res = await request(app)
+      .post('/presigned-url')
+      .send({ objectKey: '61a54a37-6c7a-466a-8100-a6b09daecded'});
+    
+    expect(res.statusCode).toBe(400);
+    expect(res.body).toHaveProperty('error');
+  });
+
+  it('Return 400 on invalid objectKey', async () => {
+    const res = await request(app)
+      .post('/presigned-url')
+      .send({ 
+        objectKey: 'invalid-objectkey',
+        contentType: 'video/quicktime',
+        objectType: 'application_video'
+      });
+    
+    expect(res.statusCode).toBe(400);
+    expect(res.body).toHaveProperty('error');
+  });
+
+  it('Return 400 on unsupported objectType', async () => {
+    const res = await request(app)
+      .post('/presigned-url')
+      .send({ 
+        objectKey: '61a54a37-6c7a-466a-8100-a6b09daecded',
+        contentType: 'video/quicktime',
+        objectType: 'something_weird'
+      });
+    
+    expect(res.statusCode).toBe(400);
+    expect(res.body).toHaveProperty('error');
+  });
+});
