f58674df6e5c7bd0360963b2739a1e14523535a6
aliona matveeva <yellalena@gmail.com>
2025-10-05 16:53:33 +0200

    presigned URL + endpoint

---
+++ aws/s3_client.js
@@ -0,0 +1,50 @@
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const config = require("config");
+
+const awsConfig = config.aws;
+const s3 = new S3Client({ region: awsConfig.aws_region });
+
+const BUCKET_PREFIXES = {
+  applications: "videos/applications",
+  // images and-or other video categories will be added later
+};
+
+const MIME_EXTENSION_MAP = {
+  "video/mp4": "mp4",
+  "video/webm": "webm",
+  "video/ogg": "ogv",
+  "video/quicktime": "mov",
+};
+
+async function generateUploadUrl(bucketPrefix, filename, contentType) {
+  if (!Object.values(BUCKET_PREFIXES).includes(bucketPrefix)) {
+    throw new Error("Invalid bucket prefix");
+  }
+
+  filename = `${filename}.${MIME_EXTENSION_MAP[contentType]}`;
+  const fullKey = buildFullObjectKey(bucketPrefix, filename);
+
+  const command = new PutObjectCommand({
+    Bucket: awsConfig.s3_bucket,
+    Key: fullKey,
+    ContentType: contentType,
+    StorageClass: "INTELLIGENT_TIERING", // Cheapest option, auto-moves to colder storage based on access patterns
+  });
+
+  const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
+  return {
+    url,
+    filename,
+  };
+}
+
+function buildFullObjectKey(bucketPrefix, filename) {
+  return `${bucketPrefix}/${filename}`;
+}
+
+module.exports = {
+  BUCKET_PREFIXES,
+  MIME_EXTENSION_MAP,
+  generateUploadUrl,
+};
