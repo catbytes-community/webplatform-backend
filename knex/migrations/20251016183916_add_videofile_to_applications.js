f4c9274a19dc96205cebd4587e4afa4f6f2afe63
aliona matveeva <yellalena@gmail.com>
2025-10-16 20:49:12 +0200

    added video_filename to applications table

---
+++ knex/migrations/20251016183916_add_videofile_to_applications.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {   
+    await knex.schema.alterTable('applications', function(table) {  
+      table.string('video_filename', 255);
+    });
+  };
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = async function(knex) {
+    await knex.schema.alterTable('applications', function(table) {
+      table.dropColumn('video_filename');
+    });  
+  };
+  
