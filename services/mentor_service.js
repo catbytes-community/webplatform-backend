eb3acfe8566c6047d063ab48f00e5f8f82240c6b
aliona matveeva <yellalena@gmail.com>
2025-10-26 09:45:46 +0100

    add missing awaits

--- services/mentor_service.js
+++ services/mentor_service.js
@@ -87,11 +87,11 @@ async function updateMentorStatus(userRoles, mentorId, status, isOwner) {
     // if admin approves mentor (pending -> active -> add mentor role)
     if(mentorData.status === MENTOR_STATUSES.pending && status === MENTOR_STATUSES.active) {
       updatedMentorId = await repo.updateMentorById(mentorId, { status });
-      assignRoleToUser(mentorData.user_id, 2);
+      await assignRoleToUser(mentorData.user_id, 2);
     } else if(status === MENTOR_STATUSES.rejected) {
       // if admin rejects mentor (change mentor status to rejected => remove mentor role)
       updatedMentorId = await repo.updateMentorById(mentorId, { status });
-      removeRoleFromUser(mentorData.user_id, 2);
+      await removeRoleFromUser(mentorData.user_id, 2);
     } else {
       // any other status changes allowed without side effect actions
       updatedMentorId = await repo.updateMentorById(mentorId, { status });
